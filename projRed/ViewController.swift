dee7e0244c42b548b9367b77abd1dc36e5070c32
davit shavtvalishvili <dttshavtvalishvili@gmail.com>
2022-08-01 17:40:32 +0400

    Initial Commit

---
+++ projRed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projRed
+//
+//  Created by დავით შავთვალიშვილი on 01.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
